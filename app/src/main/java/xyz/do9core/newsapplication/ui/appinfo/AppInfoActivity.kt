7630d2d09330fca6553d90d20bb3c30959d8baba
qiaolei <qiaolei@fenrir-tec.com>
2019-12-12 08:42:15 +0800

    Add NewsApi link to app info page.

--- app/src/main/java/xyz/do9core/newsapplication/ui/appinfo/AppInfoActivity.kt
+++ app/src/main/java/xyz/do9core/newsapplication/ui/appinfo/AppInfoActivity.kt
@@ -5,10 +5,16 @@ import xyz.do9core.newsapplication.databinding.ActivityAppInfoBinding
 import xyz.do9core.newsapplication.ui.base.BindLayout
 import xyz.do9core.newsapplication.ui.base.BindingActivity
 
+
 @BindLayout(R.layout.activity_app_info)
 class AppInfoActivity : BindingActivity<ActivityAppInfoBinding>() {
 
     override fun setupBinding(binding: ActivityAppInfoBinding) {
         binding.toolbar.setNavigationOnClickListener { onBackPressed() }
+        binding.newsApiLink.text = buildString {
+            val label = getString(R.string.app_info_special_thanks)
+            val newsApiUrl = getString(R.string.app_info_news_api_link)
+            append("$label $newsApiUrl")
+        }
     }
 }
