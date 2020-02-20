acb517cd2e6aead908d26dbb3b300345046bfd2a
qiaolei <qiaolei@fenrir-tec.com>
2020-02-20 15:57:51 +0800

    Update dependencies, use moshi replace gson

--- app/src/main/java/xyz/do9core/newsapplication/di/DataModule.kt
+++ app/src/main/java/xyz/do9core/newsapplication/di/DataModule.kt
@@ -5,7 +5,7 @@ import org.koin.android.ext.koin.androidContext
 import org.koin.dsl.bind
 import org.koin.dsl.module
 import retrofit2.Retrofit
-import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.converter.moshi.MoshiConverterFactory
 import retrofit2.create
 import xyz.do9core.newsapplication.data.db.AppDatabase
 import xyz.do9core.newsapplication.data.remote.NewsService
@@ -18,7 +18,7 @@ val DataModule = module {
     single {
         Retrofit.Builder()
             .baseUrl(API_BASE_URL)
-            .addConverterFactory(GsonConverterFactory.create())
+            .addConverterFactory(MoshiConverterFactory.create())
             .build()
     }
 }
