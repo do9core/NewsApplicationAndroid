00ca1828b2d605533c5b6ed625cebb45476f3956
qiaolei <qiaolei@fenrir-tec.com>
2020-03-02 11:40:52 +0800

    Upgrade coroutine timer

--- app/src/main/java/xyz/do9core/newsapplication/util/coroutine/CoroutineTimer.kt
+++ app/src/main/java/xyz/do9core/newsapplication/util/coroutine/CoroutineTimer.kt
@@ -2,11 +2,14 @@ package xyz.do9core.newsapplication.util.coroutine
 
 import android.os.Build
 import androidx.annotation.RequiresApi
-import androidx.lifecycle.Lifecycle
-import androidx.lifecycle.LifecycleObserver
-import androidx.lifecycle.OnLifecycleEvent
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import kotlinx.coroutines.*
+import xyz.do9core.newsapplication.util.Event
+import xyz.do9core.newsapplication.util.trigger
 import java.time.Duration
+import kotlin.coroutines.CoroutineContext
+import kotlin.coroutines.EmptyCoroutineContext
 
 private typealias TimerCallback = () -> Unit
 
@@ -14,10 +17,13 @@ private inline val Job.isInactive get() = !this.isActive
 private inline val Job.isFinished get() = this.isCancelled || this.isCompleted
 
 class CoroutineTimer(
-    private val scope: CoroutineScope,
+    coroutineContext: CoroutineContext = EmptyCoroutineContext,
     private val durationInMs: Long,
-    private val callback: TimerCallback? = null
-) : LifecycleObserver {
+    private val callback: TimerCallback
+) {
+
+    private val coroutineScope: CoroutineScope
+        = CoroutineScope(Dispatchers.Default + coroutineContext)
 
     private var _timerJob = buildTimerJob()
 
@@ -30,37 +36,50 @@ class CoroutineTimer(
         }
     }
 
-    @OnLifecycleEvent(Lifecycle.Event.ON_STOP)
     fun stop() {
         _timerJob.cancel()
     }
 
     private suspend fun timerLoop() {
         while (true) {
-            withContext(Dispatchers.Main) {
-                callback?.invoke()
-            }
+            withContext(Dispatchers.Main) { callback() }
             delay(durationInMs)
         }
     }
 
     private fun buildTimerJob(): Job {
-        return scope.launch(
-            context = Dispatchers.Default,
-            start = CoroutineStart.LAZY
-        ) {
-            timerLoop()
-        }
+        return coroutineScope.launch { timerLoop() }
     }
 }
 
+@Suppress("NOTHING_TO_INLINE")
 @RequiresApi(Build.VERSION_CODES.O)
-fun CoroutineScope.launchTimer(duration: Duration, callback: TimerCallback? = null) =
-    launchTimer(duration.toMillis(), callback)
+inline fun launchTimer(
+    coroutineContext: CoroutineContext = EmptyCoroutineContext,
+    duration: Duration,
+    noinline callback: TimerCallback
+) = launchTimer(coroutineContext, duration.toMillis(), callback)
 
-fun CoroutineScope.launchTimer(durationInMs: Long, callback: TimerCallback? = null) =
-    CoroutineTimer(
-        this,
-        durationInMs,
-        callback
-    )
+@Suppress("NOTHING_TO_INLINE")
+@RequiresApi(Build.VERSION_CODES.O)
+inline fun timerLiveData(
+    coroutineContext: CoroutineContext = EmptyCoroutineContext,
+    duration: Duration
+): LiveData<Event<Unit>> = timerLiveData(coroutineContext, duration.toMillis())
+
+fun launchTimer(
+    coroutineContext: CoroutineContext = EmptyCoroutineContext,
+    durationInMs: Long,
+    callback: TimerCallback
+) = CoroutineTimer(coroutineContext, durationInMs, callback)
+
+fun timerLiveData(
+    coroutineContext: CoroutineContext = EmptyCoroutineContext,
+    durationInMs: Long
+): LiveData<Event<Unit>> = object : MutableLiveData<Event<Unit>>() {
+
+    private val timer = launchTimer(coroutineContext, durationInMs) { this.trigger() }
+
+    override fun onActive() { timer.start() }
+    override fun onInactive() { timer.stop() }
+}
